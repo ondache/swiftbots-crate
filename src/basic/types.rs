34f1c14564a263c942d6955b6378a017aa43bb12
ondache <211723208+ondache@users.noreply.github.com>
2026-05-16 22:38:52 +0200

    allowed OneshotBot clone trait

--- src/basic/types.rs
+++ src/basic/types.rs
@@ -5,6 +5,7 @@ use crate::types::BoxFuture;
 pub type ListenerFunction<TRequest> = dyn Fn(UnboundedSender<TRequest>) -> BoxFuture<()> + Send + Sync + 'static;
 pub type HandlerFunction<TRequest> = dyn Fn(TRequest) -> BoxFuture<()> + Send + Sync + 'static;
 
+#[derive(Clone)]
 pub struct OneshotBot<TRequest> {
     pub name: Arc<String>,
     pub service: BotService<TRequest>,
